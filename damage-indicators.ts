4c9efe1546d8308a60e29fc2b319521b0bbb11aa
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2023-10-10 17:40:43 +0200

    Added custom fontsize support.

--- damage-indicators.ts
+++ damage-indicators.ts
@@ -3,7 +3,7 @@
 namespace damageIndicators {
     let indicator: Sprite
     
-    //% block="$parent show a text indicator with the text $text|| and the color $color with gravity $gravity and a initial velocity of $initialVelocity"
+    //% block="$parent show a text indicator with the text $text|| and the color $color and a fontSize of $fontSize and with gravity $gravity and a initial velocity of $initialVelocity"
     //% blockId=damageindicatorsshowindicator
     //% inlineInputMode=inline
     //% parent.shadow=variables_get
@@ -13,8 +13,10 @@ namespace damageIndicators {
     //% color.defl=1
     //% gravity.defl=500
     //% initialVelocity.defl=130
-    export function showIndicator(parent: Sprite, text: string, color?: number, gravity?: number, initialVelocity?: number) {
+    //% fontSize.defl=1
+    export function showIndicator(parent: Sprite, text: string, color?: number, gravity?: number, initialVelocity?: number, fontSize?: number) {
         indicator = textsprite.create(text, 0, color)
+        indicator.setScale(fontSize, ScaleAnchor.Middle)
         indicator.bottom = parent.y
         indicator.x = parent.x
         indicator.ay = gravity
