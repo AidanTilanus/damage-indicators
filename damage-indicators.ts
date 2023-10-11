7d047e3f03ee096a4e0fe5a0ff0b3f1bb10e319a
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2023-10-11 17:33:35 +0200

    Update damage-indicators.ts

--- damage-indicators.ts
+++ damage-indicators.ts
@@ -5,7 +5,7 @@ namespace damageIndicators {
     
     //% block="$sprite show $text|| | with color $color and fontsize $fontSize and gravity $gravity and initial velocity $initialVelocity"
     //% blockId=damageindicatorsshowindicator
-    //% inlineInputMode=inline
+    //% inlineInputMode=external
     //% sprite.shadow=variables_get
     //% sprite.defl=mySprite
     //% text.defl="-1"
