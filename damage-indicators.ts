014d6fdf8a66c309b8ae687dc18c9c844b6bf94f
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2023-10-11 15:30:52 +0200

    shortend the block

--- damage-indicators.ts
+++ damage-indicators.ts
@@ -3,26 +3,26 @@
 namespace damageIndicators {
     let indicator: Sprite
     
-    //% block="$parent show a text indicator with the text $text|| and the color $color and a fontsize of $fontSize and with gravity $gravity and a initial velocity of $initialVelocity"
+    //% block="$sprite show a indicator with text $text|| and fontsize $fontSize and gravity $gravity and initial velocity $initialVelocity"
     //% blockId=damageindicatorsshowindicator
     //% inlineInputMode=inline
-    //% parent.shadow=variables_get
-    //% parent.defl=mySprite
+    //% sprite.shadow=variables_get
+    //% sprite.defl=mySprite
     //% text.defl="-1"
     //% color.shadow="colorindexpicker"
     //% color.defl=1
     //% gravity.defl=500
     //% initialVelocity.defl=130
     //% fontSize.defl=1
-    export function showIndicator(parent: Sprite, text: string, color?: number, gravity?: number, initialVelocity?: number, fontSize?: number) {
+    export function showIndicator(sprite: Sprite, text: string, color?: number, gravity?: number, initialVelocity?: number, fontSize?: number) {
         indicator = textsprite.create(text, 0, color)
         indicator.setScale(fontSize, ScaleAnchor.Middle)
-        indicator.bottom = parent.y
-        indicator.x = parent.x
+        indicator.bottom = sprite.y
+        indicator.x = sprite.x
         indicator.ay = gravity
         indicator.vy = -initialVelocity
         indicator.vx = randint(-35, 35)
-        indicator.z = parent.z + 1
+        indicator.z = sprite.z + 1
         indicator.lifespan = randint(600, 700)
         indicator.setFlag(SpriteFlag.AutoDestroy, true)
         indicator.setFlag(SpriteFlag.Ghost, true)
