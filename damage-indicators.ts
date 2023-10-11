0828836281be80c9757c34736d3bf3f640f4f0b7
AidanTilanus <137188145+AidanTilanus@users.noreply.github.com>
2023-10-11 17:29:52 +0200

    Update pxt.json, damage-indicators.ts

--- damage-indicators.ts
+++ damage-indicators.ts
@@ -3,7 +3,7 @@
 namespace damageIndicators {
     let indicator: Sprite
     
-    //% block="$sprite show a indicator with text $text|| and fontsize $fontSize and gravity $gravity and initial velocity $initialVelocity"
+    //% block="$sprite show $text|| | with color $color and fontsize $fontSize and gravity $gravity and initial velocity $initialVelocity"
     //% blockId=damageindicatorsshowindicator
     //% inlineInputMode=inline
     //% sprite.shadow=variables_get
@@ -15,16 +15,18 @@ namespace damageIndicators {
     //% initialVelocity.defl=130
     //% fontSize.defl=1
     export function showIndicator(sprite: Sprite, text: string, color?: number, gravity?: number, initialVelocity?: number, fontSize?: number) {
-        indicator = textsprite.create(text, 0, color)
-        indicator.setScale(fontSize, ScaleAnchor.Middle)
-        indicator.bottom = sprite.y
-        indicator.x = sprite.x
-        indicator.ay = gravity
-        indicator.vy = -initialVelocity
-        indicator.vx = randint(-35, 35)
-        indicator.z = sprite.z + 1
-        indicator.lifespan = randint(600, 700)
-        indicator.setFlag(SpriteFlag.AutoDestroy, true)
-        indicator.setFlag(SpriteFlag.Ghost, true)
+        if(sprite){
+            indicator = textsprite.create(text, 0, color)
+            indicator.setScale(fontSize, ScaleAnchor.Middle)
+            indicator.bottom = sprite.y
+            indicator.x = sprite.x
+            indicator.ay = gravity
+            indicator.vy = -initialVelocity
+            indicator.vx = randint(-35, 35)
+            indicator.z = sprite.z + 1
+            indicator.lifespan = randint(600, 700)
+            indicator.setFlag(SpriteFlag.AutoDestroy, true)
+            indicator.setFlag(SpriteFlag.Ghost, true)
+        }    
     }
 }
